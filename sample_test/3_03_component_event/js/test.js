f58cc45809e3b975ef9b60cbe61fd29e33c98209
evofan <evofan@outlook.jp>
2020-11-10 00:03:37 +0900

    add 3-3 test component props and event

---
+++ sample_test/3_03_component_event/js/test.js
@@ -0,0 +1,76 @@
+﻿console.log("test 3-3 component");
+
+// Vueがロードされてグローバル変数として定義されているか確認
+console.assert(typeof Vue !== 'undefined');
+
+// ■Vueインスタンス前に定義する
+let counterButton = Vue.extend({
+    template: '<span>{{ counter }}個<button v-on:click="addToCart">追加</button></span>',
+    data: function () {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        addToCart: function () {
+            this.counter += 1;
+            this.$emit('increment'); // incrementカスタムイベントの発火
+        }
+    },
+});
+
+const vm = new Vue({
+    el: "#fruits-counter",
+    components: {
+        'counter-button': counterButton
+    },
+    data() {
+        return {
+            total: 0,
+            fruits: [
+                { name: '梨' },
+                { name: 'イチゴ' }
+            ]
+        }
+    },
+    methods: {
+        incrementCartStatus() {
+            this.total += 1;
+        }
+    }
+});
+
+
+
+// JSFIDDLEでconsoleからvmアクセスする用
+window.vm = vm;
+
+Vue.config.devtools = true;
+
+
+// ■コンポーネント間の通信
+// Vue.jsは親から子コンポーネントへのみデータを受け渡せる
+// propsというオプションを使用して通信を行う
+// 子側も状態を把握するため、eventを使用して通信を行える
+
+// ■子コンポーネントから親コンポーネントへの通信
+// カスタムイベントを使用して行う
+// 用途　　　　　　　インターフェイス
+// イベントのlisten  $on(eventName)
+// イベントのtrigger %emit(eventName)
+
+
+// ■propsとeventを使わない親子間のやり取り
+// $parentを使って可能だが、混乱をもたらすので基本使わない
+
+
+// ■親子以外のコンポーネントでデータをやり取りする
+// 例えば兄弟関係にある複数のコンポーネント同士で同じデータを共有する場合等
+// →「ストア」というオブジェクトに状態を持たせてそこで管理する＝ストアパターン
+// VueにはVuexというライブラリがあり、7章で解説
+
+
+// ■子から親のネイティブDOMイベントを取得したい場合 → .native修飾子を使う
+
+
+// ■propsの値に関して双方向バインディングを実現したい場合 → .sync修飾子を使う
---
+++ sample_test/3_03_component_props/js/test.js
@@ -0,0 +1,78 @@
+﻿console.log("test 3-3 component");
+
+// Vueがロードされてグローバル変数として定義されているか確認
+console.assert(typeof Vue !== 'undefined');
+
+// ■Vueインスタンス前に定義する
+Vue.component('fruits-item-name', {
+    props: {
+        fruitsItem: { // テンプレート内ではケバブケースに
+            type: Object,
+            required: true
+        }
+    },
+    template: `<li>{{ fruitsItem.name}}</li>`
+});
+
+
+const vm = new Vue({
+    el: "#fruits-component",
+    data() {
+        return {
+            fruitsItems: [
+                { name: '梨' },
+                { name: 'イチゴ' }
+            ]
+        }
+    }
+});
+
+
+
+// JSFIDDLEでconsoleからvmアクセスする用
+window.vm = vm;
+
+Vue.config.devtools = true;
+
+
+// ■コンポーネント間の通信
+// Vue.jsは親から子コンポーネントへのみデータを受け渡せる
+// propsというオプションを使用して通信を行う
+// 子側も状態を把握するため、eventを使用して通信を行える
+
+// ■親から子コンポーネントへのデータ伝播
+/*
+Vue.component(コンポーネント名, {
+    props : {
+        親から受け取る属性名: {
+            type: String型等,
+            default: デフォルト値,
+            required: 必須かどうあｋのブール値,
+            validator: バリデーション用の関数
+        }
+    }
+})
+// tepmplate内で「親から受け取る属性」が使える
+*/
+
+// ■実際の渡し方
+// props側にキャメルケースで「itemName」と書いた時、テンプレート側の属性名にはケバブケースで「item-name」と書く
+/*
+<div id="app">
+    <item-desc v-bind:item-name="myItem"></item-desc>
+</div >
+
+    Vue.component('item-desc', {
+        prosp: {
+            itemName: {
+                type: String
+            }
+        }
+    template: `<p>{{ item-name }}は便利です</p>`
+    });
+
+new Vue({
+    el: '#app',
+    data: { myItem: 'pen' }
+})
+*/
