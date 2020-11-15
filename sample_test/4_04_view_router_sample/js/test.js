f1a78be63a56577a402ae79cdb884913df4fd2d1
evofan <evofan@outlook.jp>
2020-11-16 01:00:56 +0900

    add 4-4 router sample app

---
+++ sample_test/4_04_view_router_sample/js/test.js
@@ -0,0 +1,51 @@
+﻿console.log("test 4-5 view routerサンプル");
+
+// Vueがロードされてグローバル変数として定義されているか確認
+console.assert(typeof Vue !== 'undefined');
+
+// ルーターの定義
+let router = new VueRouter({
+    // コンポーネントをマッピングしたルート定義を配列で返す
+    routes: [
+        {
+            path: '/top',
+            component: {
+                template: '<div>トップページです</div>'
+            }
+        },
+        {
+            path: '/users',
+            component: {
+                template: '<div>ユーザー一覧ページです</div>'
+            }
+        }
+
+    ]
+});
+
+const vm = new Vue({
+    el: "#app",
+
+    router: router,
+
+    data() {
+        return {
+            // キー：値
+        }
+    }
+});
+
+
+
+// JSFIDDLEでconsoleからvmアクセスする用
+window.vm = vm;
+
+Vue.config.devtools = true;
+
+
+// ■例として、簡易的なユーザー情報登録・閲覧が可能なSPAを作成する
+// （サーバー側のAPIについては割愛
+
+// 「トップページ」
+// 「ユーザー一覧ページ」→「ユーザー詳細ページ」
+// 「新規ユーザー登録」（ログイン無し＝/login / ログイン有り＝/users/new）
