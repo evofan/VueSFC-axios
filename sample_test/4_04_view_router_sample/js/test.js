0916ac7eec41ad067e3baa47d6c251c3df69d051
evofan <evofan@outlook.jp>
2020-12-13 09:45:29 +0900

    fix

--- sample_test/4_04_view_router_sample/js/test.js
+++ sample_test/4_04_view_router_sample/js/test.js
@@ -3,6 +3,10 @@
 // Vueがロードされてグローバル変数として定義されているか確認
 console.assert(typeof Vue !== 'undefined');
 
+let userList = {
+    template: '#user-list'
+}
+
 // ルーターの定義
 let router = new VueRouter({
     // コンポーネントをマッピングしたルート定義を配列で返す
@@ -15,9 +19,7 @@ let router = new VueRouter({
         },
         {
             path: '/users',
-            component: {
-                template: '<div>ユーザー一覧ページです</div>'
-            }
+            component: userList
         }
 
     ]
@@ -49,3 +51,11 @@ Vue.config.devtools = true;
 // 「トップページ」
 // 「ユーザー一覧ページ」→「ユーザー詳細ページ」
 // 「新規ユーザー登録」（ログイン無し＝/login / ログイン有り＝/users/new）
+
+// ■4-4-2 APIによるデータ通信
+// ★SPAでは、ページ遷移をした際に、APIを通じて取得したデータをUIに表示する処理が頻出する
+// Vue RouterでAjaxによる非同期通信を行う場合は、
+// ★createdとwatchを使って実装するのが一般的
+// watchは算出プロパティを汎用的にしたVueコンポーネントのオプション
+// （watchは$routeを監視して、ルートに変更がある度に処理を呼び出す）
+// ★watchによる$routeの検出は頻出するパターン
